080dfcb2e2c682a7b96cc4e4a3e6b571aaad642e
Jith Dev <jithdev@xminds.com>
2024-10-14 13:15:46 +0530

    Add project files.

---
+++ ProductCRUD/DAL/Product_DAL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using ProductCRUD.Models;
+
+namespace ProductCRUD.DAL
+{
+    public class Product_DAL
+    {
+        //string constring = ConfigurationManager.ConnectionStrings["ProductConnection"].ToString();
+        public List<Product>GetProducts()
+        {
+            List<Product> productlist = new List<Product>();
+
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ProductConnection"].ToString()))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "Get_Products";
+                SqlDataAdapter sqlDA = new SqlDataAdapter(command); 
+                DataTable dtProducts = new DataTable();
+
+                connection.Open();
+                sqlDA.Fill(dtProducts);
+                connection.Close();
+
+                foreach (DataRow dr in dtProducts.Rows)
+                {
+                    productlist.Add(new Product
+                    {
+                        ProductID = Convert.ToInt32(dr["ProductID"]),
+                        ProductName = dr["ProductName"].ToString(),
+                        Category = dr["Category"].ToString(),
+                    });
+                }
+            }
+            return productlist;
+        }
+    }
+}
---
+++ ProductCRUD/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductCRUD.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        [DisplayName("Prodcut Name")]
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+    }
+}
